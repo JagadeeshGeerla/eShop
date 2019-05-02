d691723e315b0d6ee3843e263376df9093d16ad8
jagadeeshgeerla <jagadeesh.geerla@gmail.com>
2019-05-02 23:57:07 +0530

    Implemented Generic Repository

--- eShop/eShop.Core/Models/Product.cs
+++ eShop/eShop.Core/Models/Product.cs
@@ -8,10 +8,8 @@ using System.Threading.Tasks;
 
 namespace eShop.Core.Models
 {
-    public class Product
+    public class Product : BaseEntity
     {
-        public string Id { get; set; }
-
         [StringLength(20)]
         [DisplayName("Product Name")]
         public string Name { get; set; }
@@ -20,12 +18,7 @@ namespace eShop.Core.Models
         [Range(0,1000)]
         public decimal Price { get; set; }
         public string Category { get; set; }
-        public string Image { get; set; }
-
-        public Product()
-        {
-            this.Id = Guid.NewGuid().ToString();
-        }
+        public string Image { get; set; }        
 
     }
 }
